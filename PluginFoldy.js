4be050395d35b229fa123759d24b36d388ecb611
Josh Terrell <joshterrell805@gmail.com>
2017-11-25 17:06:02 +0000

    create

---
+++ PluginFoldy.js
@@ -0,0 +1,84 @@
+var Foldy = Util.createSingleton('Foldy', function() {
+});
+
+Foldy.plugin = function plugin(app) {
+  setInterval(Foldy.getInstance().update.bind(Foldy.getInstance()), 200);
+  SimplePersonalSite.Util.linkCss('PluginFoldy.css');
+};
+
+/**
+ * Look for elements that should have foldy behavior added, and add the behavior.
+ */
+Foldy.prototype.update = function update() {
+  $('div.foldy-div').each(function() {
+    var start = $(this);
+    var end = start.nextAll('#'+start.attr('id')+'-close');
+    if (end.length == 0) {
+      console.error('No closing element found for id=' + start.attr('id'));
+    } else {
+      // place elements between start and end as children of start.
+      var between = start.nextUntil(end);
+      end.remove();
+      start.append(between);
+
+      // add toggle dongy (show/hide)
+      var toggle = $('<p id="' + start.attr('id') + '-toggle' +
+          '"><a class="foldy-hide">hide</a><a>show</a></p>');
+      toggle.insertBefore(start);
+      if (!start.hasClass('foldy-hide')) {
+        Foldy.toggle(toggle.children('a'));
+      }
+
+      // do stuff when show/hide is clicked
+      toggle.click(function() {
+        var children = toggle.children('a');
+        Foldy.toggle(children);
+        Foldy.toggle(start);
+      });
+    }
+    start.removeClass('foldy-div');
+    start.addClass('foldy-div-found');
+  });
+
+  $('.foldy-next').each(function() {
+    var marker = $(this);
+
+    // find next element
+    var itr = marker;
+    while (!itr.is('body') && itr.next().length === 0) {
+      itr = $(itr.parent());
+    }
+    var next = itr.next();
+
+    if (next.length == 0) {
+      console.error('No element after', marker);
+    } else {
+      // add toggle dongy (show/hide)
+      var toggle = $('<a class="foldy-hide">show</a><a>hide</a>');
+      marker.append(toggle);
+      if (marker.hasClass('foldy-hide')) {
+        Foldy.toggle(toggle);
+        Foldy.toggle(next);
+      }
+
+      // do stuff when show/hide is clicked
+      toggle.click(function() {
+        Foldy.toggle(toggle);
+        Foldy.toggle(next);
+      });
+    }
+    marker.removeClass('foldy-next');
+    marker.removeClass('foldy-hide');
+  })
+};
+
+/**
+ * Toggle the visibility of the foldy elements.
+ */
+Foldy.toggle = function toggle(elms) {
+  elms = $(elms);
+  var hidden = elms.filter('.foldy-hide');
+  var shown = elms.not('.foldy-hide');
+  hidden.removeClass('foldy-hide');
+  shown.addClass('foldy-hide');
+};
---
+++ README.md
@@ -0,0 +1,40 @@
+## PluginInBrowserEdits
+This plugin allows you to show/hide elements in [SimplePersonalSite](https://github.com/SimplePersonalSite/SimplePersonalSite).
+
+## Install
+1. follow directions to setup a [SimplePersonalSite](https://github.com/SimplePersonalSite/SimplePersonalSite).
+1. `cd` to the root directory of your site
+1. symlink `PluginFoldy.css` and `PluginFoldy.js`
+1. include `PluginFoldy.js` as a script in `index.html` **below** `SimplePersonalSite.js`
+  - `<script src='PluginFoldy.js'></script>`
+1. Tell the app to initialize the plugin by passing `PluginFoldy.plugin` to `app.run()` in `index.html`
+  - if you only have one plugin, this looks like:
+  - `app.run([PluginFoldy.plugin])`
+
+## Usage
+### `foldy-div`
+Show/hide an entire section of content.
+
+1. Above the content you want to show/hide, add `<div id='your-id-here' class='foldy-div'></div>`
+2. Below your content, add `<div id='your-id-here-close'></div>`
+
+
+##### `foldy-div-found`
+If you want to style your foldy-divs, you can add style to the class `foldy-div-found`. I like to add margin to the left so content get indented (css): `.foldy-div-found { margin-left: 15px }`
+
+##### Why not just put the content inside of a div?
+Some markdown renderers don't render markdown inside divs very well. Adding a div above and below content should work well for most markdown renderers.
+
+### `foldy-next`
+Show/hide the next element.
+
+If there is no next element, recursively keep search up to parents until a next element can be found, or the `body` is reached.
+
+1. Before the element you want to show/hide, add `<span class='foldy-next'></span>`
+
+### `foldy-hide`
+If you want the element to start off as hidden instead of shown, just add `foldy-hide` as a class to the foldy element.
+
+Examples:
+- `<span class='foldy-next foldy-hide'></span>`
+- `<span id='cool-element-yes' class='foldy-div foldy-hide'></span>`
